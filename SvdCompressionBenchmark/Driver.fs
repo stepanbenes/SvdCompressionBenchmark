69ee7fcc9c1c932e30105a56082232e9ac1ee4e1
Štěpán Beneš <benes.stepan@gmail.com>
2016-09-22 01:32:49 +0200

    First working binding to native redsvd library

---
+++ SvdCompressionBenchmark/Driver.fs
@@ -0,0 +1,38 @@
+﻿namespace SvdCompressionBenchmark
+
+module RedSvdDriver =
+
+    module Native =
+    
+        open System.Runtime.InteropServices
+        open Microsoft.FSharp.NativeInterop
+    
+        [<DllImport("redsvd.dll")>]
+        extern void ComputeSvdExact(double[] inputMatrix_RowMajor, int numberOfRows, int numberOfColumns, [<Out>] double[] singularValues, [<Out>] double[] U_VT_ColumnMajor)
+
+        [<DllImport("redsvd.dll")>]
+        extern void ComputeSvdRandomized(double[] inputMatrix_RowMajor, int numberOfRows, int numberOfColumns, int rank, [<Out>] double[] singularValues, [<Out>] double[] U_VT_ColumnMajor)
+    
+    open MathNet.Numerics.LinearAlgebra
+
+    let computeSvdExact (A : Matrix<double>) = 
+        
+        let rank = min A.RowCount A.ColumnCount
+        
+        let singularValues = Array.zeroCreate rank
+        let U_VT_ColumnMajor = Array.zeroCreate (A.RowCount * rank + rank * A.ColumnCount)
+        
+        Native.ComputeSvdExact(A.ToRowWiseArray(), A.RowCount, A.ColumnCount, singularValues, U_VT_ColumnMajor)
+        
+        (CreateMatrix.Diagonal singularValues, CreateMatrix.Dense (A.RowCount, rank, U_VT_ColumnMajor.[.. (A.RowCount * rank - 1)]), CreateMatrix.Dense (rank, A.ColumnCount, U_VT_ColumnMajor.[(A.RowCount * rank) ..]))
+
+
+    let computeSvdRandomized rank (A : Matrix<double>) = 
+        
+        let singularValues = Array.zeroCreate rank
+        let U_VT_ColumnMajor = Array.zeroCreate (A.RowCount * rank + rank * A.ColumnCount)
+        
+        Native.ComputeSvdRandomized(A.ToRowWiseArray(), A.RowCount, A.ColumnCount, rank, singularValues, U_VT_ColumnMajor)
+
+        (CreateMatrix.Diagonal singularValues, CreateMatrix.Dense (A.RowCount, rank, U_VT_ColumnMajor.[.. (A.RowCount * rank - 1)]), CreateMatrix.Dense (rank, A.ColumnCount, U_VT_ColumnMajor.[(A.RowCount * rank) ..]))
+
--- SvdCompressionBenchmark/Program.fs
+++ SvdCompressionBenchmark/Program.fs
@@ -1,7 +1,54 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿namespace SvdCompressionBenchmark
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+module Program =
+
+    open MathNet.Numerics.LinearAlgebra
+    open RedSvdDriver
+
+    let calculate f input =
+        (* SVD *)
+        let singularValues, u, vt = f input
+        
+        printfn "Singular Value Decomposition of:"
+        printfn "%A" input
+        
+        printfn "singular values:"
+        printfn "%A" singularValues
+        printfn "U:"
+        printfn "%A" u
+        printfn "VT:"
+        printfn "%A" vt
+
+        let assembled = u * singularValues * vt
+
+        printfn "assembled matrix:"
+        printfn "%A" assembled
+
+    let benchmark count =
+        
+        for i in 1 .. count do
+            let randomInput = DenseMatrix.randomStandard<double> 10 10
+            calculate computeSvdExact randomInput
+            calculate (computeSvdRandomized 9) randomInput
+
+    [<EntryPoint>]
+    let main argv = 
+        
+        benchmark (System.Int32.Parse argv.[0])
+
+        // random matrix with standard distribution:
+        //let m6 = DenseMatrix.randomStandard<double> 3 4
+        
+//        let input = matrix [[ 1.0;  -2.0;   3.0];
+//                            [ 5.0;   8.0;  -1.0];
+//                            [ 2.0;   1.0;   1.0];
+//                            [-1.0;   4.0;  -3.0]]
+        
+        
+
+        (* Randomized SVD *)
+        //let singularValues, uv, t = RedSvdDriver.computeSvdRandomized input 2
+
+        0 // return an integer exit code
+
+    
