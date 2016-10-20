d508fec0fffbedbe5676377867eee0c496cb05d8
Štěpán Beneš <benes.stepan@gmail.com>
2016-10-20 23:53:43 +0200

    Add comparison of matrix MSE and MSE calculated from matrix's singular values

--- SvdCompressionBenchmark/Program.fs
+++ SvdCompressionBenchmark/Program.fs
@@ -2,52 +2,71 @@
 
 module Program =
 
+    open MathNet.Numerics
     open MathNet.Numerics.LinearAlgebra
     open RedSvdDriver
 
-    let calculate f input =
+    let calculate f (input : Matrix<double>) =
         (* SVD *)
         let singularValues, u, vt = f input
         
-        printfn "Singular Value Decomposition of:"
-        printfn "%A" input
-        
-        printfn "singular values:"
-        printfn "%A" singularValues
-        printfn "U:"
-        printfn "%A" u
-        printfn "VT:"
-        printfn "%A" vt
+//        printfn "Singular Value Decomposition of:"
+//        printfn "%A" input
+//        
+//        printfn "singular values:"
+//        printfn "%A" singularValues
+//        printfn "U:"
+//        printfn "%A" u
+//        printfn "VT:"
+//        printfn "%A" vt
 
-        let assembled = u * singularValues * vt
+        let reconstructed : Matrix<double> = u * singularValues * vt
 
-        printfn "assembled matrix:"
-        printfn "%A" assembled
+//        printfn "assembled matrix:"
+//        printfn "%A" reconstructed
+
+        let mse = Distance.MSE(input.ToColumnWiseArray(), reconstructed.ToColumnWiseArray())
+        let psnr = 10.0 * log10 (255.0**2.0 * mse)
+
+        printfn "MSE: %A" mse
+        printfn "PSNR: %A" psnr
 
     let benchmark count =
         
         for i in 1 .. count do
-            let randomInput = DenseMatrix.randomStandard<double> 10 10
+            let randomInput = DenseMatrix.randomStandard<double> 100 100
             calculate computeSvdExact randomInput
-            calculate (computeSvdRandomized 9) randomInput
+            calculate (computeSvdRandomized 100) randomInput
 
     [<EntryPoint>]
     let main argv = 
         
-        benchmark (System.Int32.Parse argv.[0])
+        benchmark 100
 
         // random matrix with standard distribution:
         //let m6 = DenseMatrix.randomStandard<double> 3 4
         
-//        let input = matrix [[ 1.0;  -2.0;   3.0];
-//                            [ 5.0;   8.0;  -1.0];
-//                            [ 2.0;   1.0;   1.0];
-//                            [-1.0;   4.0;  -3.0]]
+        let input = matrix [[ 1.0;  -2.0;   3.0];
+                            [ 5.0;   8.0;  -1.0];
+                            [ 2.0;   1.0;   1.0];
+                            [-1.0;   4.0;  -3.0]]
         
         
 
-        (* Randomized SVD *)
-        //let singularValues, uv, t = RedSvdDriver.computeSvdRandomized input 2
+        let singularValues, uv, t = RedSvdDriver.computeSvdExact input
+
+        let sqr x = x * x
+
+        let mn = float(input.RowCount) * float(input.ColumnCount)
+
+        let mse1 = (input.ToColumnWiseArray() |> Array.map sqr |> Array.sum) / mn
+        let mse2 = (singularValues.Diagonal().Enumerate() |> Seq.map sqr |> Seq.sum) / mn
+
+        printfn "MSE1: %A" mse1
+        printfn "MSE2: %A" mse2
+        printfn "Equals: %A" (mse1 = mse2)
+
+        System.Console.ReadLine() |> ignore
 
         0 // return an integer exit code
 
